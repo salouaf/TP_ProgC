f00a7caa644cfd01d4d5a22313bf482b350bf7fe
salouaf <s.florand@cfacampusmontsouris.fr>
2026-01-29 11:00:28 +0100

    sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 
 int main() {
-printf("la taille d'un char : %zu octets\n",sizeof(char));
-printf("la taille d'un short : %zu octets\n",sizeof(short));
-printf("la taille d'un int : %zu octets\n",sizeof(int));
-printf("la taille d'un long int : %zu octets\n",sizeof(long int));
-printf("la taille d'un long long int : %zu octets\n",sizeof(long long int));
-printf("la taille d'un float : %zu octets\n",sizeof(float));
-printf("la taille d'un double : %zu octets\n",sizeof(double));
-printf("la taille d'un long double : %zu octets\n",sizeof(long double));
-return 0;
+ /* Types char */
+    printf("signed char : %zu octet(s)\n", sizeof(signed char));
+    printf("unsigned char : %zu octet(s)\n", sizeof(unsigned char));
+
+    /* Types short */
+    printf("signed short : %zu octet(s)\n", sizeof(signed short));
+    printf("unsigned short : %zu octet(s)\n", sizeof(unsigned short));
+
+    /* Types int */
+    printf("signed int : %zu octet(s)\n", sizeof(signed int));
+    printf("unsigned int : %zu octet(s)\n", sizeof(unsigned int));
+
+    /* Types long int */
+    printf("signed long int : %zu octet(s)\n", sizeof(signed long int));
+    printf("unsigned long int : %zu octet(s)\n", sizeof(unsigned long int));
+
+    /* Types long long int */
+    printf("signed long long int : %zu octet(s)\n", sizeof(signed long long int));
+    printf("unsigned long long int : %zu octet(s)\n", sizeof(unsigned long long int));
+
+    /* Types réels (pas de signed / unsigned) */
+    printf("float : %zu octet(s)\n", sizeof(float));
+    printf("double : %zu octet(s)\n", sizeof(double));
+    printf("long double : %zu octet(s)\n", sizeof(long double));
+
+    return 0;
 }
+
 
