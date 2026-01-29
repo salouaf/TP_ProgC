a9ed4c9bf3ccaf5a9ce70701c9a79c99be1b90d5
salouaf <s.florand@cfacampusmontsouris.fr>
2026-01-29 12:39:45 +0100

    opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+int a = 16;
+int b = 3;
+  
+/*addition*/
+printf("a + b = %d\n", a + b);
+
+/*soustraction*/
+printf("a - b = %d\n", a - b);
+
+/*multiplication*/
+printf("a * b = %d\n", a * b);
+
+/*division*/
+printf("a - b = %f\n", (float)a / b);   /*Pour un résultat décimal, il faut convertir en float*/
+
+/*reste*/
+printf("reste de la divison a / b = %d\n", a % b);
+
+/*Utilisez un opérateur de comparaison pour vérifier si a est égal à b et affichez le résultat sous forme de booléen*/
+printf(" a = b = %d\n", a == b);  /* %d car entier puisque 0 = false, 1 = true*/
+  
+/*  ?  %s pour afficher vrai ou faux*/
+printf("a == b : %s\n", (a == b) ? "true" : "false");
+
+/*Utilisez un opérateur de comparaison pour vérifier si a est supérieur à b et affichez le résultat sous forme de booléen.*/
+printf(" a > b = %d\n", a > b);
+printf("a > b : %s\n", (a > b) ? "true" : "false");
+  
+return 0;
+}
+
 
 
